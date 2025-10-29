3c7ebc88fccfbe8ce9c519add6b2cd477668a34c
brkim2008 <brkim2008@hanmail.net>
2025-10-29 10:08:06 +0900

    집에서 작업완료 1029

--- src/test/HelloGitHub.java
+++ src/test/HelloGitHub.java
@@ -5,6 +5,8 @@ public class HelloGitHub {
 	public static void main(String[] args) {
 		System.out.println("1.프로젝트생성");
 		System.out.println("2.내용수정1");
+		System.out.println("3.집에서 작업");
+
 
 	}
 
